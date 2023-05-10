09c46cf0607fa12c9ef27652f41a89b3b16d64e0
Azi312 <aziz260110@gmail.com>
2023-05-11 05:39:29 +0600

    add image

--- src/components/widgets/User.tsx
+++ src/components/widgets/User.tsx
@@ -110,7 +110,6 @@ const User: FC<UserProps> = ({ userId, avatarUrl }) => {
 
 			<Divider />
 
-			{/* FOURTH ROW */}
 			<Box p='1rem 0'>
 				<Typography fontSize='1rem' color={main} fontWeight='500' mb='1rem'>
 					Social Profiles
